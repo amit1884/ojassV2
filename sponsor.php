c261061574131a3af3c6b03606b38962b5dea155
“gadarsh555” <gadarsh555@gmail.com>
2020-01-04 23:31:46 +0530

    font solved

--- sponsor.php
+++ sponsor.php
@@ -4,7 +4,20 @@ include_once 'nav.php';
 <body>
 <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
 	<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
-<link rel = "stylesheet" href = "css/style_sponsors.css"><br><br><br>
+
+<link rel = "stylesheet" href = "css/style_sponsors.css">
+<style>
+  .imgBx img {
+  -webkit-filter: grayscale(90%); /* Safari 6.0 - 9.0 */
+  filter: grayscale(90%);
+}
+.imgBx img:hover{
+-webkit-filter: grayscale(0%);
+filter: grayscale(0%);
+}
+</style>
+
+<br><br><br>
 <center>
 <h2 class="text-center" ><span class="page_title" style="color:white; font-family: 'Play', sans-serif;font-size:50px;text-decoration: underline;">Sponsors</span></h2></center>
 <?php 
