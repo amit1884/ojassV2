ee097939134b9414ff6ec9b074eb18e524ccf49e
“gadarsh555” <gadarsh555@gmail.com>
2020-01-04 22:16:17 +0530

    new footer

--- footer/footer-1.php
+++ footer/footer-1.php
@@ -1,6 +1,5 @@
 <!-- ##### Contact Area Start ##### -->
 <link rel="stylesheet" href="footer/style.css">
-<link rel="stylesheet" href="footer/footer-1.css">
 
 <section class="contact-area section-padding-100 bg-img bg-overlay bg-fixed has-bg-img" style="background-image: url(footer/img/bg-img/bg-2.jpg);">
         <div class="container">
@@ -52,49 +51,31 @@
     <!-- ##### Contact Area End ##### -->
 
     <!-- ##### Footer Area Start ##### -->
-    
-    <!-- <footer class="footer-area"> -->
-    <div id="animate-area">
+    <footer class="footer-area">
         <div class="container">
             
             <div class="row d-flex flex-wrap align-items-center">
                 <div class="col-12 col-md-6">
+                    
                     <a href="#"><img src="footer/img/core-img/logo.png" alt=""></a>
-                   <!--  -->
+                    <p class="copywrite-text"><a href="#"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
                 </div>
 
-               <!--  <div class="col-12 col-md-6"> -->
-                  
-                <!-- </div> -->
+                <div class="col-12 col-md-6">
+                    <div class="footer-nav">
+                        <ul>
+                            <li><a href="profile.php">Home</a></li>
+                            <li><a href="shop.php">Shop</a></li>
+                            <li><a href="collection.php">Collection</a></li>
+                            <li><a href="contact.php">Contact</a></li>
+                        </ul>
+                    </div>
+                </div>
             </div>
         </div>
-        
-                   <center style="color:#524F4D;">
-                   <ul class="list-inline" >
-              <p style="font-size:18px;font-family: 'Play', sans-serif;font-color:#524F4D;">Ojass'19</p>
-            </ul>
-                   <ul class="list-inline" style="list-style-type:none">
-            <li><i class="fa fa-map-marker text-light" style="font-size: 16px;"><a style="font-family: 'Jura', sans-serif;color:#524F4D;"> NIT Jamshedpur</a></i></li>
-            <li><a class="fa fa-envelope text-light" style="font-size: 16px;"><a style="font-family: 'Jura', sans-serif;color:#524F4D;" href="mailto:ojass@nitjsr.ac.in"> ojass@nitjsr.ac.in</a> </i></li>
-          </ul>
-                    <div>
-                        <ul>
-                        <li>           
-            <a href="https://www.facebook.com/Ojassnitjamshedpur/" ><i class="fa fa-facebook-official" style="font-size: 36px;color:#524F4D;" target="_blank"></i></a>
-            <a href="https://www.youtube.com/user/ojassnitjsr" class="fa fa-youtube-square" style="font-size: 36px; margin-left: 12px;color:#524F4D;" target="_blank"></a>
-            <a href="https://www.instagram.com/ojass_techfest/" class="fa fa-instagram" style="font-size: 36px; margin-left: 12px;color:#524F4D;" target="_blank"></a>
-            <br>
-            
-            <p style="font-size: 14px; margin: 0;"> <a  style="text-decoration:none;color:#524F4D;" href="developers.php">© Ojass Web Team</a> </p>
-            
-        </li>
-        
-                        </ul>
-                    
-                    </center>
-                    </div>
-    <!-- </footer> -->
-    
+    </footer>
     <!-- ##### Footer Area Start ##### -->
      <!-- ##### All Javascript Script ##### -->
     <!-- jQuery-2.2.4 js -->
@@ -106,7 +87,7 @@
     <!-- All Plugins js -->
     <script src="footer/js/plugins/plugins.js"></script>
     <!-- Active js -->
-    <!-- <script src="footer/js/active.js"></script> -->
+    <script src="footer/js/active.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    
+    </footer>
     
