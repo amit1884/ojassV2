c1647125e3522a982a700c5af95ab938bb1d1484
ankitc-11 <geniusankit10.ac@gmail.com>
2020-01-04 23:14:33 +0530

    Update nav.php

--- nav.php
+++ nav.php
@@ -10,7 +10,7 @@
     <script src ="js/jquery.js"></script>
     <style>
     	.nav-colored {
-    	 	background-color :  #c4ebed ;
+    	 	background-color :  #b6caec ;
     	 	animation-name: fadeIn;
     	 	animation-duration: 0.4s;
     	 }
