259e7db3bdaad2d11c769157bf360d5c40ea9c7e
“gadarsh555” <gadarsh555@gmail.com>
2020-01-06 18:45:02 +0530

    new contact page

--- contact.php
+++ contact.php
@@ -1,68 +1,20 @@
-<?php include_once 'nav.php';?>
 
-<!-- ##### Contact Area Start ##### -->
-<link rel="stylesheet" href="footer/style.css">
-<link rel="stylesheet" href="footer/footer-1.css">
- <section class="contact-area section-padding-100 bg-img bg-overlay bg-fixed has-bg-img" style="background-image: url(footer/img/bg-img/bg-1.jpg);">
-        <div class="container">
-            <div class="row">
-                <div class="col-12">
-                    <div class="section-heading white">
-                        <h2>Get In Touch</h2>
-                    </div>
-                </div>
-            </div>
 
-            <div class="row">
-                <div class="col-12">
-                     
-                    <div class="contact-form-area">
-                    <form name="mail" method="post" action="mail.php" >
-                            <div class="row">
-                                <div class="col-lg-4">
-                                    <div class="form-group">
-                                        <input type="text" class="form-control" id="name" placeholder="Name" name="fname">
-                                    </div>
-                                </div>
-                                <div class="col-lg-4">
-                                    <div class="form-group">
-                                        <input type="email" class="form-control" id="email" placeholder="E-mail" name="femail">
-                                    </div>
-                                </div>
-                                <div class="col-lg-4">
-                                    <div class="form-group">
-                                        <input type="text" class="form-control" id="subject" placeholder="Subject" name="subject">
-                                    </div>
-                                </div>
-                                <div class="col-12">
-                                    <div class="form-group">
-                                        <textarea name="message" class="form-control" id="message" cols="30" rows="10" placeholder="Message" ></textarea>
-                                    </div>
-                                </div>
-                                <div class="col-12 text-center">
-                                    <button class="btn oneMusic-btn mt-30" name="sendmail" type="submit">Send <i class="fa fa-angle-double-right"></i></button>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-    <!-- ##### Contact Area End ##### -->
-    <?php include_once 'footer2.php';?>
-    <!-- ##### Footer Area Start ##### -->
-     <!-- ##### All Javascript Script ##### -->
-    <!-- jQuery-2.2.4 js -->
-    <script src="footer/js/jquery/jquery-2.2.4.min.js"></script>
-    <!-- Popper js -->
-    <script src="footer/js/bootstrap/popper.min.js"></script>
-    <!-- Bootstrap js -->
-    <script src="footer/js/bootstrap/bootstrap.min.js"></script>
-    <!-- All Plugins js -->
-    <script src="footer/js/plugins/plugins.js"></script>
-    <!-- Active js -->
-    <!-- <script src="footer/js/active.js"></script> -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    
-    
+<!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"> -->
+<link rel="stylesheet" href="css/contact-2.css"><?php include_once 'nav.php';?>
+<div class="container">
+  
+  <div id="cloud-intro">
+    <div class="contact-box">  
+    <center><h1 style="color:darkred;font-size:7vh;font-weight:600;">Get in touch</h1></center>
+    <form>      
+  <input name="name" type="text" class="feedback-input" placeholder="Name" />   
+  <input name="email" type="text" class="feedback-input" placeholder="Email" />
+  <textarea name="text" class="feedback-input" placeholder="Comment"></textarea>
+  <input type="submit" value="SUBMIT"/>
+</form>
+    </div>
+  </div>
+</div>
+<?php include_once 'footer2.php';?>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
