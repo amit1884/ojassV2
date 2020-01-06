6d7ca3942d7b5778205ec9beee48599f9fc66f87
“gadarsh555” <gadarsh555@gmail.com>
2020-01-06 20:10:10 +0530

    margin

--- contact.php
+++ contact.php
@@ -24,7 +24,7 @@
   </div>
   
 </div>
-<?php include_once 'footer2.php';?>
-</body>
 
+</body>
+<?php include_once 'footer2.php';?>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
