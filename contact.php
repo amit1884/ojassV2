45fa16705e30bd4463e08da7f30c43b5cc672472
“gadarsh555” <gadarsh555@gmail.com>
2020-01-06 19:49:12 +0530

    margin

--- contact.php
+++ contact.php
@@ -1,7 +1,7 @@
 
 
-<!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"> -->
-<link rel="stylesheet" href="css/contact-2.css">
+<!--  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"> -->
+<link rel="stylesheet" href="css/contact.css">
 <?php include_once 'nav.php';?>
 <body>
   <!-- bhrthr6 -->
@@ -9,6 +9,7 @@
 <div class="container">
   
   <div id="cloud-intro">
+    
     <div class="contact-box">  
     <center><h1 style="color:darkred;font-size:7vh;font-weight:600;">Get in touch</h1></center>
     <form >      
@@ -17,9 +18,11 @@
   <textarea name="text" class="feedback-input" placeholder="Comment"></textarea>
   <input type="submit" value="SUBMIT"/>
 </form>
-<br><br>
+
     </div>
+    
   </div>
+  
 </div>
 <?php include_once 'footer2.php';?>
 </body>
