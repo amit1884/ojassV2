7378b1c75ef76d98aee5e498584934a29763fb10
“gadarsh555” <gadarsh555@gmail.com>
2020-01-09 00:50:30 +0530

    scroll to top button

---
+++ scroll_icon/scroll.php
@@ -0,0 +1,6 @@
+<link rel="stylesheet" href="scroll.css">
+<link rel="stylesheet" href="scroll.css">
+<span class="arrow">up</span>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<script src="scroll.js"></script>
+<script src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/15979/footer-inject.jss"></script>
