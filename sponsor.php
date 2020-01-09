f161e2c05cb0582dd5539f6d3e548db09d9d30eb
Amit Raj <49913746+amit1884@users.noreply.github.com>
2020-01-09 21:46:24 +0530

    aos added in sponsor

--- sponsor.php
+++ sponsor.php
@@ -4,7 +4,7 @@ include_once 'nav.php';
 <body>
 <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
   <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
-
+<script src ="js.jquery,js"></script>
 <link rel = "stylesheet" href = "css/style_sponsors.css">
 <style>
   .white-panel {
@@ -34,16 +34,10 @@ filter: grayscale(0%);
 
 <br><br><br><br>
 <center> <img src ="svg/sponsors.svg" width ="300" height ="40"></center><br>
-<!-- <center> -->
-<!-- <h2 class="text-center" ><span class="page_title" style="color:white; font-family: 'Play', sans-serif;font-size:50px;text-decoration: underline;">Sponsors</span></h2></center> -->
-<?php 
-// include_once 'back.php';
-?>
-</div>
 <br><br> 
 
       <div class='sponsor-card'>
-        <div class="imgS white-panel">
+        <div class="imgS white-panel"  data-aos='fade-left'>
             <img src="images/sponsors/oyo.png" alt="images">
         </div>
       </div>
@@ -51,7 +45,7 @@ filter: grayscale(0%);
         <br><br> 
 
        <div class='sponsor-card'>
-         <div class="imgS white-panel">
+         <div class="imgS white-panel"  data-aos='flip-left'>
             <img src="images/sponsors/Picture2.png" alt="images">
          </div>
            </div>
@@ -59,30 +53,30 @@ filter: grayscale(0%);
        <br><br>
 
        <div class='sponsor-card'>
-         <div class="imgS white-panel">
+         <div class="imgS white-panel"  data-aos='zoom-in'>
             <img src="images/sponsors/Picture3.png" alt="images">
          </div>
       </div>
        <br><br>
        <div class='sponsor-card'>
-         <div class="imgS white-panel">
+         <div class="imgS white-panel"  data-aos='flip-right'>
             <img src="images/sponsors/Picture4.png" alt="images">
          </div>
       </div>
        <br><br>
        <div class='sponsor-card'>
-         <div class="imgS white-panel">
+         <div class="imgS white-panel"  data-aos='fade-down'>
             <img src="images/sponsors/Picture5.png" alt="images">
          </div>
       </div>
        <br><br>
        <div class='sponsor-card'>
-         <div class="imgS white-panel">
+         <div class="imgS white-panel"  data-aos='slide-right'>
             <img src="images/sponsors/Picture6.png" alt="images">
          </div>
       </div>
        <br><br>
-       <div class='sponsor-card'>
+       <div class='sponsor-card'  data-aos='zoom-in-right'>
          <div class="imgS white-panel">
             <img src="images/sponsors/Picture14.png" alt="images">
          </div>
@@ -92,3 +86,9 @@ filter: grayscale(0%);
        
 <?php include_once 'footer2.php';?>
 
+  
+<script type="text/javascript">
+	AOS.init({
+  	duration:1200,
+  });
+</script>
