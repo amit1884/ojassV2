6e744bc7b707ac79dfa0418e48cf467863f7b1b8
ankitc-11 <geniusankit10.ac@gmail.com>
2020-01-10 16:34:40 +0530

    event animation changed

--- event.php
+++ event.php
@@ -8,6 +8,7 @@
     <script type ="text/javascript" src ="js/jquery.js"></script>
     <link href="https://fonts.googleapis.com/css?family=Roboto+Slab&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.2/animate.min.css">
+    <link rel="stylesheet" type="text/css" href="css\animation.css">
     <title>OJASS|EVENTS</title>
 </head>
 <body style ="background-color:#f5fdf8">
@@ -48,31 +49,32 @@
     <center><img src= "svg/events.svg" width= "250"height="50"></center><br><br>
     <div class ="main-event eve" id ="m-eve">
 
-    <a href ="event/index.php?branch=ARTHASHASTRA"target ="_blank"><div class='major_events animated rollIn ' ><img src='svg\eventsIcons\arthashastra.svg'><h5>Arthashastra</h5></div>
-    <a href ="event/index.php?branch=AAKRITI"target ="_blank"><div class='major_events animated bounceIn ' ><img src='svg\eventsIcons\akriti.svg'><h5>Aakriti</h5></div>
-    <a href ="event/index.php?branch=AVARTAN"target ="_blank"><div class='major_events animated zoomIn ' ><img src='svg\eventsIcons\avartan.svg'><h5>Avartan</h5></div>
+    <a href ="event/index.php?branch=ARTHASHASTRA"target ="_blank"><div class='major_events animatable bounceInLeft ' ><img src='svg\eventsIcons\arthashastra.svg'><h5>Arthashastra</h5></div>
+    <a href ="event/index.php?branch=AAKRITI"target ="_blank"><div class='major_events animatable bounceIn' ><img src='svg\eventsIcons\akriti.svg'><h5>Aakriti</h5></div>
+    <a href ="event/index.php?branch=AVARTAN"target ="_blank"><div class='major_events animatable bounceInRight ' ><img src='svg\eventsIcons\avartan.svg'><h5>Avartan</h5></div>
    
-    <a href ="event/index.php?branch=Circuit_House"target ="_blank"><div class='major_events animated slideInLeft' ><img src='svg\eventsIcons\circuit_house.svg'><h5>Circuit-House</h5></div>
-    <a href ="event/index.php?branch=Deus-X-Machina"target ="_blank"><div class='major_events animated rubberBand' ><img src='svg\eventsIcons\deus_x_machina.svg'><h5>Deus-X-Machina</h5></div>
-    <a href ="event/index.php?branch=Live CS"target ="_blank"><div class='major_events animated slideInright' ><img src='svg\eventsIcons\live_cs.svg'><h5>Live CS</h5></div>
+    <a href ="event/index.php?branch=Circuit_House"target ="_blank"><div class='major_events animatable bounceInRight' ><img src='svg\eventsIcons\circuit_house.svg'><h5>Circuit-House</h5></div>
+    <a href ="event/index.php?branch=Deus-X-Machina"target ="_blank"><div class='major_events animatable fadeInDown' ><img src='svg\eventsIcons\deus_x_machina.svg'><h5>Deus-X-Machina</h5></div>
+    <a href ="event/index.php?branch=Live CS"target ="_blank"><div class='major_events animatable bounceInLeft' ><img src='svg\eventsIcons\live_cs.svg'><h5>Live CS</h5></div>
     <div style= "display:inline-block">
-    <a href ="event/index.php?branch=Neo Drishti"target ="_blank"><div class='major_events animated bounceInRight'><img src='svg\eventsIcons\neodrishti.svg'><h5>Neo Drishti</h5></div>
-    <a href ="event/index.php?branch=No Ground Zone"target ="_blank"><div class='major_events animated fadeInUpBig'><img src='svg\eventsIcons\no_ground_zone.svg'><h5>No Groud Zone</h5></div>
-    <a href ="event/index.php?branch=NSCET"target ="_blank"><div class='major_events animated bounceInRight'><img src='svg\eventsIcons\nscet.svg'><h5>NSCET</h5></div> 
+    <a href ="event/index.php?branch=Neo Drishti"target ="_blank"><div class='major_events animatable fadeInDown'><img src='svg\eventsIcons\neodrishti.svg'><h5>Neo Drishti</h5></div>
+    <a href ="event/index.php?branch=No Ground Zone"target ="_blank"><div class='major_events animatable fadeInUp'><img src='svg\eventsIcons\no_ground_zone.svg'><h5>No Groud Zone</h5></div>
+    <a href ="event/index.php?branch=NSCET"target ="_blank"><div class='major_events animatable fadeInDown'><img src='svg\eventsIcons\nscet.svg'><h5>NSCET</h5></div> 
     </div>
-    <a href ="event/index.php?branch=Paraphernalia"target ="_blank"><div class='major_events animated rotateIn'><img src='svg\eventsIcons\paraphernalia.svg'><h5>Paraphernalia</h5></div>
-    <a href ="event/index.php?branch=Prayas"target ="_blank"><div class='major_events animated fadeInUpBig'><img src='svg\eventsIcons\prayas.svg'><h5>Prayas</h5></div>
-    <a href ="event/index.php?branch=Produs"target ="_blank"><div class='major_events animated slideInLeft'><img src='svg\eventsIcons\produs.svg'><h5>Produs</h5></div>
+    <a href ="event/index.php?branch=Paraphernalia"target ="_blank"><div class='major_events animatable bounceIn'><img src='svg\eventsIcons\paraphernalia.svg'><h5>Paraphernalia</h5></div>
+    <a href ="event/index.php?branch=Prayas"target ="_blank"><div class='major_events animatable bounceIn'><img src='svg\eventsIcons\prayas.svg'><h5>Prayas</h5></div>
+    <a href ="event/index.php?branch=Produs"target ="_blank"><div class='major_events animatable bounceIn'><img src='svg\eventsIcons\produs.svg'><h5>Produs</h5></div>
    
-    <a href ="event/index.php?branch=Rise of Machines"target ="_blank"><div class='major_events animated rollIn'><img src='svg\eventsIcons\rise_of_machines.svg'><h5>Rise of Machines</h5></div>
-    <a href ="event/index.php?branch=School Events"target ="_blank"><div class='major_events animated bounceInRigh'><img src='svg\eventsIcons\school_events.svg'><h5>School Events</h5></div>
-    <a href ="event/index.php?branch=Armageddon"target ="_blank"><div class='major_events animated bounceInLeft'><img src='svg\eventsIcons\armageddon.svg'><h5>Armageddon</h5></div>
+    <a href ="event/index.php?branch=Rise of Machines"target ="_blank"><div class='major_events animatable bounceInRight'><img src='svg\eventsIcons\rise_of_machines.svg'><h5>Rise of Machines</h5></div>
+    <a href ="event/index.php?branch=School Events"target ="_blank"><div class='major_events animatable bounceIn'><img src='svg\eventsIcons\school_events.svg'><h5>School Events</h5></div>
+    <a href ="event/index.php?branch=Armageddon"target ="_blank"><div class='major_events animatable bounceInLeft'><img src='svg\eventsIcons\armageddon.svg'><h5>Armageddon</h5></div>
     <div style= "display:inline-block">
-    <a href ="event/index.php?branch=Silicon Valley"target ="_blank"><div class='major_events animated rollIn'><img src='svg\eventsIcons\silicon_valley.svg'><h5>Silicon Valley</h5></div>
-    <a href ="event/index.php?branch=VishwaCodegenesis"target ="_blank"><div class='major_events animated rubberBand'><img src='svg\eventsIcons\vishwacodegenesis.svg'><h5>Vishwacode Genesis</h5></div>
-    <a href ="event/index.php?branch=Exposicion"target ="_blank"><div class='major_events animated bounceInRight'><img src='svg\eventsIcons\exposicion.svg'><h5>Exposicion</h5></div>
+    <a href ="event/index.php?branch=Silicon Valley"target ="_blank"><div class='major_events animatable bounceInLeft'><img src='svg\eventsIcons\silicon_valley.svg'><h5>Silicon Valley</h5></div>
+    <a href ="event/index.php?branch=VishwaCodegenesis"target ="_blank"><div class='major_events animatable fadeInUp'><img src='svg\eventsIcons\vishwacodegenesis.svg'><h5>Vishwacode Genesis</h5></div>
+    <a href ="event/index.php?branch=Exposicion"target ="_blank"><div class='major_events animatable bounceInRight'><img src='svg\eventsIcons\exposicion.svg'><h5>Exposicion</h5></div>
     </div>
 </div>
 <?php include_once 'footer2.php';?>
+<script type="text/javascript" src='js\animation.js'></script>
 </body>
 </html>
