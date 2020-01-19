a2033242b22fd6b61324eec526490085697804cf
“gadarsh555” <gadarsh555@gmail.com>
2020-01-19 13:46:29 +0530

    footer

---
+++ footer.php
@@ -0,0 +1,25 @@
+<style>
+
+
+</style>
+
+<center style="color:white; background-image: linear-gradient(to right top, #af6fda, #9966ca, #845cb9, #7053a9, #5d4998);">
+     <ul class="list-inline" >
+     <p style="font-size:25px;font-family: 'Play', sans-serif;font-color:white;font-weight:900;margin-right:5%;"><img src="logo1.png" width="40" height="40">Ojass'20</p>
+     </ul>
+     <ul class="list-inline" style="list-style-type:none">
+     <li><i class="fa fa-map-marker " style="font-size: 16px;"></i><a style="font-family: 'Jura', sans-serif;color:white;font-weight:700;"> NIT Jamshedpur</a></li>
+     <li><a class="fa fa-envelope " style="font-size: 16px;"><a style="font-family: 'Jura', sans-serif;color:white;font-weight:700;" href="mailto:ojass@nitjsr.ac.in"> ojass@nitjsr.ac.in</a> </i></li>
+     </ul>
+        
+          <ul style="margin:0rem;list-style:none;margin-right:3rem;">
+          <li>           
+           <a href="https://www.facebook.com/Ojassnitjamshedpur/" ><img  src="images/facebook.png" width="35" height="35" style="font-size: 36px;color:white;font-weight:700;" target="_blank"></a>
+            <a href="https://www.youtube.com/user/ojassnitjsr" ><img  src="images/youtube.png" width="35" height="35"  style="font-size: 36px; margin-left: 12px;color:white;" target="_blank"></a>
+            <a href="https://www.instagram.com/ojass_techfest/" ><img  src="images/instagram.png" width="35" height="35" style="font-size: 36px; margin-left: 12px;color:white;" target="_blank"></a>
+           <p style="font-size: 18px; margin: 0;">© <a  style="text-decoration:none;color:#0B98C0;" href="developers.php"> Ojass Web Team</a> </p>
+            
+        </li>
+        </ul>
+                        
+</center>
