f1b509efe5fe64dd4fe710a04b5750d288138980
ankitc-11 <geniusankit10.ac@gmail.com>
2020-01-22 20:49:30 +0530

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,5 +1,8 @@
 <link href ="css/bootstrap.min.css" rel ="stylesheet">
 <style>
+  .foot{
+    margin: 5px auto;
+  }
     /* background-image: linear-gradient(to bottom, #0b060c, #1a0b21, #220f36, #25124e, #1e1768); */
 #oj-20{
     color:#413e66;
@@ -12,12 +15,12 @@
 </style>
 <div class ="container-fluid foo-ter" style ="background:#7ea2e2">
 <div class ="row text-center">
-<div class ="col-12"><img src ="images/newlogo.png" width ="40"height="40"><span id ="oj-20">OJASS'20</span></div><br><br>
-<div class ="col-12"><i class="fa fa-map-marker " style="font-size: 16px;color:#000"></i><a style="font-family: 'Jura', sans-serif;color:#000;font-weight:700;"> NIT Jamshedpur</a></div><br>
-<div class ="col-12"><i class="fa fa-envelope " style="font-size: 16px;color:#000"></i><a style="font-family: 'Jura', sans-serif;color:#000;font-weight:700;" href="mailto:ojass@nitjsr.ac.in"> ojass@nitjsr.ac.in</a></div><br>
-<div class ="col-12"><a href="https://www.facebook.com/Ojassnitjamshedpur/" ><img  src="images/facebook.png" width="35" height="35" style="font-size: 36px;color:#413E66;font-weight:700;" target="_blank"></a>
+<div class ="col-12"><img src ="images/OJ'20 LOGO pr 3.png" style='width:50%; margin:auto; height:auto;'><!-- <span id ="oj-20">OJASS'20</span> --></div><br><br>
+<!-- <div class ="col-12"><i class="fa fa-map-marker " style="font-size: 16px;color:#000"></i><a style="font-family: 'Jura', sans-serif;color:#000;font-weight:700;"> NIT Jamshedpur</a></div><br>
+<div class ="col-12"><i class="fa fa-envelope " style="font-size: 16px;color:#000"></i><a style="font-family: 'Jura', sans-serif;color:#000;font-weight:700;" href="mailto:ojass@nitjsr.ac.in"> ojass@nitjsr.ac.in</a></div><br> -->
+<div class ="col-12 foot" style="margin-top: -10px;"><a href="https://www.facebook.com/Ojassnitjamshedpur/" ><img  src="images/facebook.png" width="35" height="35" style="font-size: 36px;color:#413E66;font-weight:700;" target="_blank"></a>
 <a href="https://www.youtube.com/user/ojassnitjsr" ><img  src="images/youtube.png" width="35" height="35"  style="font-size: 36px; margin-left: 12px;color:#413E66;" target="_blank"></a>
 <a href="https://www.instagram.com/ojass_techfest/" ><img  src="images/instagram.png" width="35" height="35" style="font-size: 36px; margin-left: 12px;color:#413E66;" target="_blank"></a></div>
-<div class ="col-12"> <p style="font-size: 18px; margin: 0;color:#000">© <a  style="text-decoration:none;color:#000;" href="developers.php"> Ojass Web Team</a> </p></div>
+<div class ="col-12 foot"> <p style="font-size: 18px; margin: 0;color:#000">© <a  style="text-decoration:none;color:#000;" href="developers.php"> Ojass Web Team</a> </p></div>
 </div>
 </div>
