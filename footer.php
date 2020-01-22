72773501d428c9a75b4f15faebf704c6bbb48c86
ankitc-11 <geniusankit10.ac@gmail.com>
2020-01-22 21:05:49 +0530

    footer done

--- footer.php
+++ footer.php
@@ -15,10 +15,10 @@
 </style>
 <div class ="container-fluid foo-ter" style ="background:#7ea2e2">
 <div class ="row text-center">
-<div class ="col-12"><img src ="images/OJ'20 LOGO pr 3.png" style='width:60%; margin:auto; height:auto;'><!-- <span id ="oj-20">OJASS'20</span> --></div><br><br>
+<div class ="col-12 foot"><img src ="images/OJ'20 LOGO.png" style='width:80%; margin:auto; height:auto;'><!-- <span id ="oj-20">OJASS'20</span> --></div><br><br>
 <!-- <div class ="col-12"><i class="fa fa-map-marker " style="font-size: 16px;color:#000"></i><a style="font-family: 'Jura', sans-serif;color:#000;font-weight:700;"> NIT Jamshedpur</a></div><br>
 <div class ="col-12"><i class="fa fa-envelope " style="font-size: 16px;color:#000"></i><a style="font-family: 'Jura', sans-serif;color:#000;font-weight:700;" href="mailto:ojass@nitjsr.ac.in"> ojass@nitjsr.ac.in</a></div><br> -->
-<div class ="col-12 foot" style="margin-top: -16px;"><a href="https://www.facebook.com/Ojassnitjamshedpur/" ><img  src="images/facebook.png" width="35" height="35" style="font-size: 36px;color:#413E66;font-weight:700;" target="_blank"></a>
+<div class ="col-12 foot"><a href="https://www.facebook.com/Ojassnitjamshedpur/" ><img  src="images/facebook.png" width="35" height="35" style="font-size: 36px;color:#413E66;font-weight:700;" target="_blank"></a>
 <a href="https://www.youtube.com/user/ojassnitjsr" ><img  src="images/youtube.png" width="35" height="35"  style="font-size: 36px; margin-left: 12px;color:#413E66;" target="_blank"></a>
 <a href="https://www.instagram.com/ojass_techfest/" ><img  src="images/instagram.png" width="35" height="35" style="font-size: 36px; margin-left: 12px;color:#413E66;" target="_blank"></a></div>
 <div class ="col-12 foot"> <p style="font-size: 18px; margin: 0;color:#000">© <a  style="text-decoration:none;color:#000;" href="developers.php"> Ojass Web Team</a> </p></div>
