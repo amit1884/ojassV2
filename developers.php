f0edf2e201fa32f0dbcaf6b518e2bb9de82e861e
Masuu-2105 <masoodmuhammad365@gmail.com>
2020-01-30 00:06:31 +0530

    Minor change

--- developers.php
+++ developers.php
@@ -78,7 +78,7 @@
 			<h4>Ankit Choudhary, ME</h4>
 		</div>
 		<div class='member' data-aos='fade-right'>
-			<img src="images/developers/masood.jpeg"height="280">
+			<img src="images/developers/Masood.jpeg"height="280">
 			<h4>Masood, EE</h4>
 		</div>
 	</div>
